40b4ec889ff4315bb0e291c67baa5b2eb4d41c25
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2024-02-01 18:46:03 +0530

    priceapp.py

--- priceapp.py
+++ priceapp.py
@@ -13,27 +13,28 @@ def myntra_price(style_id):
     try:
         res_4 = rq.Session().get(f'https://www.myntra.com/foundation-and-primer/swiss-beauty/swiss-beauty-long-lasting-makeup-fixer-natural-spray---aloe-vera-with-vitamin-e-50-ml/{style_id}/buy', headers=headerss)
         soup_4 = BeautifulSoup(res_4.text, 'lxml')
-        script = None
-        for s in soup_4.find_all("script"):
-            if 'pdpData' in s.text:
-                script = s.get_text(strip=True)
-                break
-        aa = json.loads(script[script.index('{'):])
-        st.write(aa)
-        price_look_4 = aa['pdpData']['selectedSeller']['discountedPrice']
-        st.write(price_look_4)
-        return price_look_4
-    except:
-        return 'OOS'
+        st.write(soup_4)
+#         script = None
+#         for s in soup_4.find_all("script"):
+#             if 'pdpData' in s.text:
+#                 script = s.get_text(strip=True)
+#                 break
+#         aa = json.loads(script[script.index('{'):])
+#         st.write(aa)
+#         price_look_4 = aa['pdpData']['selectedSeller']['discountedPrice']
+#         st.write(price_look_4)
+#         return price_look_4
+#     except:
+#         return 'OOS'
 
-def main():
-    st.title("Myntra Price Tracker")
+# def main():
+#     st.title("Myntra Price Tracker")
 
-    style_ids = ['15580238','15580024']  # Add more Style IDs as needed
+#     style_ids = ['15580238','15580024']  # Add more Style IDs as needed
     
-    for style_id in style_ids:
-        price = myntra_price(style_id)
-        st.success(f"Myntra Price for Style ID {style_id}: {price}")
+#     for style_id in style_ids:
+#         price = myntra_price(style_id)
+#         st.success(f"Myntra Price for Style ID {style_id}: {price}")
 
 if __name__ == "__main__":
     main()
